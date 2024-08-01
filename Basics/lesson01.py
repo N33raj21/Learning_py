b49a4ddb05ed86dbba7945e42f29068da5de43b3
N33raj21 <9436264521n@gmail.com>
2024-08-02 00:44:48 +0530

    tutorial on python

---
+++ Basics/lesson01.py
@@ -0,0 +1,36 @@
+# print('Hello world')
+# print(1+38)
+# a = 'Neeraj Kumar'
+# print(a)
+
+# this is comment
+# Data Type: Integer,float,Boolean
+# strings are a sequence of character enclosed in quotation marks
+# Boolean: a value can either be true or false
+
+# list: an ordered collection of values, such as [1,2,3] or ["apple","banana"]
+
+# dictionary: a collection of key-values, such as {name: "john","age":42}
+
+
+# we don't need to define the datatype it itself can detect it
+
+# x = 3.0/5
+# y = 4.5/5
+# z= x+y  "variables"
+# print(z)
+# print(type(z))
+
+# defining a list
+
+fruit = ["apple","kiwi","avacado","coconut"]
+nums = ['1','2','3']
+print(nums)
+print(fruit[1])
+
+# assignning mutiple variables t0gether
+
+x,y,z = '1','2','3'
+print(x)
+print(y)
+print(z)
---
+++ Basics/lesson02.py
@@ -0,0 +1,24 @@
+# class Human:
+#     def _init_(self, name, age): 
+#     self.name = name
+#     self.age = age
+
+# def need(self):
+#     print("ooh yes")
+
+
+#     human =  Human('Neeraj',22)
+
+#     print(human.name)
+#     print(human.age)
+#     human.need() 
+
+
+def greet(name):
+    print("Hello, " + name + "!")
+
+greet("Neeraj")
+
+x = "Welcome to"
+y = "matrix (a world full of rat race)"
+print(x + " "+y)
